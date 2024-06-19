ab8c56f776d6a1bc9ed833898a34567a99c84b8a
Ahmad Shraby <ahmed.sharaby@paysky.io>
2024-06-19 11:24:56 +0300

    Initial Commit

---
+++ SGT-CC/SGT_CCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SGT_CCApp.swift
+//  SGT-CC
+//
+//  Created by AhmadShraby on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SGT_CCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
